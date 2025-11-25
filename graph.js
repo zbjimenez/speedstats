4ce3cb3a2ab47a8bd34511262f664a10d32e246b
zbjimenez <zbjimenez@up.edu.ph>
2025-11-25 15:10:02 +0800

    Load function

--- graph.js
+++ graph.js
@@ -1,13 +1,15 @@
-const FIG = document.getElementsByTagName('figure')[0];
+function main() {
+  const FIG = document.getElementsByTagName('figure')[0];
 
-Plotly.newPlot( figure, [
-  {
-    x: [1, 2, 3, 4],
-    y: [2, 4, 8, 16]
-  },
-  {
-    margin: {t: 0}
-  },
-]);
+  Plotly.newPlot( figure, [
+    {
+      x: [1, 2, 3, 4],
+      y: [2, 4, 8, 16]
+    },
+    {
+      margin: {t: 0}
+    },
+  ]);
 
-console.log(Plotly.BUILD)
+  console.log(Plotly.BUILD);
+}
